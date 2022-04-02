100843391e29e4fc9330dd360a134c23c5dab269
Huy Hoang <huyhoang10032000@gmail.com>
2022-04-03 00:54:48 +0700

    temporatory fix logic

--- index.js
+++ index.js
@@ -16,32 +16,74 @@ app.use(
     limit: "50mb",
   })
 );
+
+function spliceSlice(str, index, count, add) {
+  if (index < 0) {
+    index = str.length + index;
+    if (index < 0) {
+      index = 0;
+    }
+  }
+
+  return str.slice(0, index) + (add || "") + str.slice(index + count);
+}
+
 app.post("/input", upload.none(), (req, res) => {
   //   const readStream = fs.createReadStream(path.resolve(__dirname, "file.txt"));
   let bufferList = [];
-//   console.log(req.body.value)
+  //   console.log(req.body.value)
   if (!req.body.value) {
     res.render("list", {
       data: [],
     });
     return;
   }
-  const child_process = spawn("node", ["calculateViewerProcess.js"]);
+  //   const child_process = spawn("node", ["calculateViewerProcess.js"]);
   //   readStream.on("data", (data) => {
   //     bufferList.push(data);
   //   });
 
   //   readStream.on("end", () => {
-  child_process.stdin.end(req.body.value);
+  //   child_process.stdin.end(req.body.value);
   //   });
 
-  child_process.stdout.on("data", (data) => {
-	  console.log(data.toString());
-    // res.json({ data: JSON.parse(data.toString()) });
-    res.render("list", {
-      data: JSON.parse(data.toString()),
-    });
+  const startString = "chat_sidebar_contact_rankings";
+  const index1 =
+    req.body.value.indexOf("chat_sidebar_contact_rankings") +
+    startString.length +
+    ":[".length +
+    1;
+  let newString = req.body.value.slice(index1);
+  let result = [];
+  let count = 0;
+  try {
+    while (newString.indexOf('{"status"') !== -1) {
+      let start = newString.indexOf('{"status"');
+      let end = newString.indexOf('"}}}') + 4;
+      const info = JSON.parse(
+        newString.slice(0, newString.indexOf('"}}}') + 4)
+      );
+      result.push(info);
+      newString = spliceSlice(newString, start, end + 1, "");
+      //   if (count < 300) count++;
+      //   else break;
+    }
+    // return console.log(JSON.stringify(result));
+  } catch (e) {
+    console.log(e);
+    // return console.log(e.message);
+  }
+  res.render("list", {
+    data: result,
   });
+
+  //   child_process.stdout.on("data", (data) => {
+  //     console.log(data.toString());
+  //     // res.json({ data: JSON.parse(data.toString()) });
+  //     res.render("list", {
+  //       data: JSON.parse(data.toString()),
+  //     });
+  //   });
 });
 app.get("/", upload.none(), (req, res) => {
   res.render("index", {});
